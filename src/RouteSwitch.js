e758724574f645d866d3782bae74d643b58b8e22
Clyde <noneclyde@gmail.com>
2023-03-06 09:40:03 +0200

    Refactor Index page's name to a more understandable

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Main from "./pages/Main";
 
 export default function RouteSwitch() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={ <Index /> }></Route>
+                <Route path="/" element={ <Main /> }></Route>
             </Routes>
         </BrowserRouter>
     )
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import "../styles/pageContainer.css";
 import PageContainer from "../components/PageContainer";
 
-export default function Index() {
+export default function Main() {
     return (
         <div>
             <Navbar />
