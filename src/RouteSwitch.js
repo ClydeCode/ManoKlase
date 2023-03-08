004395c6caef52a1b17663a020f689680e81906f
Clyde <noneclyde@gmail.com>
2023-03-08 15:58:28 +0200

    Add empty News page

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import News from "./pages/News";
 
 export default function RouteSwitch() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <Main /> }></Route>
+                <Route path="/Naujienos" element={ <News /> }></Route>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/News.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import PageContainer from "../components/PageContainer";
+
+
+export default function News() {
+    return (
+        <div>
+            <Navbar />
+            <PageContainer title="Naujienos" />
+        </div>
+    )
+}
