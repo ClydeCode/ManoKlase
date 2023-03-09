700031b432d68017a223d69806d46f86df322f5e
Clyde <noneclyde@gmail.com>
2023-03-09 20:26:43 +0200

    Add content to News page

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,11 +1,20 @@
 import Navbar from "../components/Navbar";
 import PageTitle from "../components/PageTitle";
+import "../styles/news.css";
+import NewsCard from "../components/NewsCard";
 
 export default function News() {
     return (
         <div>
             <Navbar />
             <PageTitle title="Naujienos" />
+            <div className="news-page-content">
+                <p id="datetime">2023-03</p>
+                <div className="news-page-content-cards">
+                    <NewsCard reverse={false} />
+                    <NewsCard reverse={true} />
+                </div>
+            </div>
         </div>
     )
 }
