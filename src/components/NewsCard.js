d83a93b137148a2e68c6456b377d378215cc2d1b
Clyde <noneclyde@gmail.com>
2023-03-09 20:27:03 +0200

    Add NewsCard component

---
+++ src/components/NewsCard.js
@@ -0,0 +1,33 @@
+import "../styles/newsCard.css";
+
+export default function NewsCard(props) {
+    if(!props.reverse) {
+        return (
+            <div className="news-card">
+                <div className="news-card-text">
+                    <p id="title">Europos kalbų diena</p>
+                    <p id="desc">
+                        Rugsėjo 26 dieną gimnazijos aktų
+                        salėje vyko renginys, skirtas
+                        Europos kalbų dienai
+                    </p>
+                </div>
+                <div className="empty-image-container"></div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="news-card news-card-reverse">
+                <div className="empty-image-container"></div>
+                <div className="news-card-text">
+                    <p id="reverse-title">Europos kalbų diena</p>
+                    <p id="reverse-desc">
+                        Rugsėjo 26 dieną gimnazijos aktų
+                        salėje vyko renginys, skirtas
+                        Europos kalbų dienai
+                    </p>
+                </div>
+            </div>
+            )
+    }
+}
