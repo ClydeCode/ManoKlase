8464047d6652997d2e7c00e56814eedac5bd14a3
Clyde <noneclyde@gmail.com>
2023-03-17 13:57:52 +0200

    Add algorithm for handleScroll method

--- src/pages/News.js
+++ src/pages/News.js
@@ -5,15 +5,25 @@ import NewsCard from "../components/NewsCard";
 import {useRef, useState} from "react";
 
 export default function News() {
-    const [date, setDate] = useState("2023-03");
-    const cards = useRef([]);
+    const [date, setDate] = useState("2023-09");
+    const index = useRef(0);
+    const data = [{"position": 220, "date": "2023-06"}, {"position": 460, "date": "2023-03"}];
 
     function handleScroll(e) {
-        if (e.target.scrollTop >= 532) {
-            setDate("2023-07");
+        if (index.current > 0) {
+            if (e.target.scrollTop <= data[index.current].position) {
+                setDate(data[index.current - 1].date);
+                index.current = index.current - 1;
+            }
         }
 
-        console.log(e.target.scrollTop)
+        if (data[index.current + 1] != null)
+            if (e.target.scrollTop >= data[index.current + 1].position) {
+                setDate(data[index.current + 1].date);
+                index.current = index.current + 1;
+            }
+
+        console.log(index);
     }
 
     return (
