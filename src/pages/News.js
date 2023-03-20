ffca3339ea948d41ce43aae4341b1c09224493f3
Clyde <noneclyde@gmail.com>
2023-03-20 13:54:32 +0200

    Add complete logic of scrollbar

--- src/pages/News.js
+++ src/pages/News.js
@@ -6,26 +6,47 @@ import {useRef, useState, useEffect} from "react";
 
 export default function News() {
     const [news, setNews] = useState([]);
-    const data = [{"position": 0, "date": "2023-06"}, {"position": 30, "date": "2023-03"}, {"position": 520, "date": "2023-01"}];
-    const [date, setDate] = useState(data[0].date);
+    const positionsAndDates = useRef([]);
+    const [date, setDate] = useState();
+    const scrollDiv = useRef();
 
     const fetchData = () => {
         return fetch(process.env.REACT_APP_WEBAPI + "news")
             .then(response => response.json())
-            .then(data => setNews(data));
+            .then(data => data.sort((a, b) => Date.parse(a.createdDate) > Date.parse(b.createdDate) ? -1 : 1))
+            .then(sortedData => setNews(sortedData));
     }
 
     function handleScroll(e) {
-        data.forEach(item => {
+        positionsAndDates.current.forEach(item => {
             if (e.target.scrollTop >= item.position) {
                 setDate(item.date);
             }
         })
     }
 
+    function getPositionsAndDates() {
+        let arr = [];
+
+        for (let n = 0; n < scrollDiv.current.childNodes.length; n++) {
+            const element = scrollDiv.current.childNodes[n];
+            const date = element.id.substring(0, 7);
+
+            arr.push({"position": element.offsetTop - 266, "date": date})
+        }
+
+        positionsAndDates.current = arr;
+        setDate(positionsAndDates.current[0].date);
+    }
+
     useEffect(() => {
-        fetchData();
+        fetchData()
     }, []);
+
+    useEffect(() => {
+        if (scrollDiv.current.childNodes.length > 0)
+            getPositionsAndDates();
+    }, [scrollDiv.current]);
 
     return (
         <div>
@@ -33,13 +54,14 @@ export default function News() {
             <PageTitle title="Naujienos" />
             <div className="news-page-content">
                 <p id="datetime">{date}</p>
-                <div className="news-page-content-cards" onScroll={handleScroll}>
+                <div className="news-page-content-cards" onScroll={handleScroll} ref={scrollDiv}>
                     {news.map((item, index) => {
                         return <NewsCard
                             key={index}
                             title={item.title}
                             description={item.description}
-                            reverse={!index % 2}
+                            createdDate={item.createdDate}
+                            reverse={index % 2}
                         />
                     })}
                 </div>
