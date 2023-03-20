921768b42c9b789bd3ca0b6cddd1a85abe4bc54d
Clyde <noneclyde@gmail.com>
2023-03-20 11:26:34 +0200

    Rework algorithm

--- src/pages/News.js
+++ src/pages/News.js
@@ -2,29 +2,30 @@ import Navbar from "../components/Navbar";
 import PageTitle from "../components/PageTitle";
 import "../styles/news.css";
 import NewsCard from "../components/NewsCard";
-import {useRef, useState} from "react";
+import {useRef, useState, useEffect} from "react";
 
 export default function News() {
-    const index = useRef(0);
-    const data = [{"position": 120, "date": "2023-06"}, {"position": 320, "date": "2023-03"}, {"position": 520, "date": "2023-01"}];
-    const [date, setDate] = useState(data[index.current].date);
+    const [news, setNews] = useState([]);
+    const data = [{"position": 0, "date": "2023-06"}, {"position": 30, "date": "2023-03"}, {"position": 520, "date": "2023-01"}];
+    const [date, setDate] = useState(data[0].date);
+
+    const fetchData = () => {
+        return fetch(process.env.REACT_APP_WEBAPI + "news")
+            .then(response => response.json())
+            .then(data => setNews(data));
+    }
 
     function handleScroll(e) {
-        if (index.current > 0) {
-            if (e.target.scrollTop <= data[index.current].position) {
-                setDate(data[index.current - 1].date);
-                index.current = index.current - 1;
+        data.forEach(item => {
+            if (e.target.scrollTop >= item.position) {
+                setDate(item.date);
             }
-        }
-
-        if (data[index.current + 1] != null)
-            if (e.target.scrollTop >= data[index.current + 1].position) {
-                setDate(data[index.current + 1].date);
-                index.current = index.current + 1;
-            }
-
-        console.log(index);
+        })
     }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <div>
@@ -32,11 +33,15 @@ export default function News() {
             <PageTitle title="Naujienos" />
             <div className="news-page-content">
                 <p id="datetime">{date}</p>
-                <div className="news-page-content-cards" onScroll={(e) => handleScroll(e)}>
-                    <NewsCard reverse={false} />
-                    <NewsCard reverse={true} />
-                    <NewsCard reverse={false} />
-                    <NewsCard reverse={true} />
+                <div className="news-page-content-cards" onScroll={handleScroll}>
+                    {news.map((item, index) => {
+                        return <NewsCard
+                            key={index}
+                            title={item.title}
+                            description={item.description}
+                            reverse={!index % 2}
+                        />
+                    })}
                 </div>
             </div>
         </div>
