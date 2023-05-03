43cf03c512369d40a96b9c788039d31909c96d83
Clyde <noneclyde@gmail.com>
2023-05-03 20:42:13 +0300

    Add data

--- src/pages/Class.js
+++ src/pages/Class.js
@@ -10,58 +10,122 @@ export default function Class() {
             <PageTitle title="Klasė" />
             <div className="class-page-content-center">
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Inga Jakubonienė"
+                    desc="Gyventi reikia taip, kad atsisukus atgal nebūtų gėda, kas ir koks tu buvai"
                 />
             </div>
             <div className="class-page-content">
                 <ClassCard
-                    title="Vardas Pavarde"
+                    title="Augustas Baršauskas"
+                    desc="מי שיקרא את זה יהיה תרנגול"
+                />
+                <ClassCard
+                    title="Laurynas Bulovas"
+                    desc="Today's pain is tomorrow's power"
+                />
+                <ClassCard
+                    title="Darija Grybytė"
+                    desc="tyngėjau sugalvoti citatą"
+                />
+                <ClassCard
+                    title="Simona Jasukaitytė"
+                    desc="Nori pusryčių į lovą? Miegok virtuvėje..."
+                />
+                <ClassCard
+                    title="Deimantas Kairys"
+                    desc="Esu 5 minutėmis jaunesnis..."
+                />
+                <ClassCard
+                    title="Danielė Kairytė"
+                    desc="...Geriausios 5 minutės gyvenime"
+                />
+                <ClassCard
+                    title="Gabrielė Kavaliauskaitė"
+                    desc="When you reach the end of your rope, tie a knot in it and hang on"
+                />
+                <ClassCard
+                    title="Austėja Kubiliūtė"
+                    desc="tyngėjau sugalvoti citatą"
+                />
+                <ClassCard
+                    title="Nedas Kukoris"
+                    desc="rūkymo zonoje"
+                />
+                <ClassCard
+                    title="Aida Lauraitytė"
+                    desc="“tyngėjau sugalvoti citatą"
+                />
+                <ClassCard
+                    title="Sandra Magelinskaitė"
+                    desc="一切都是相連的，直到你找到它並斷開與靈魂之旅相連的一切——桑德拉"
+                />
+                <ClassCard
+                    title="Domantas Matas"
+                    desc="tyngėjau sugalvoti citatą"
+                />
+                <ClassCard
+                    title="Ugnė Meliūnaitė"
+                    desc="Lengviau gauti atleidimą negu leidimą😉"
+                />
+                <ClassCard
+                    title="Irūna Merkelytė"
+                    desc="“Sunku išbūti žmogumi paprastu” -OG Version"
+                />
+                <ClassCard
+                    title="Arnas Povilonis"
+                    desc="pp"
+                />
+                <ClassCard
+                    title="Viktorija Pučekaitė"
+                    desc="tyngėjau sugalvoti citatą"
+                />
+                <ClassCard
+                    title="Aušrius Rumbutis"
+                    desc="jei durniai galėtų skraidyti, ši mokykla būtų oro uostas"
+                />
+                <ClassCard
+                    title="Domas Sargevičius"
+                    desc="Svarbiausia, kad bandymas matytųsi"
+                />
+                <ClassCard
+                    title="Meida Smailytė"
+                    desc="You never know"
+                />
+                <ClassCard
+                    title="Eligijus Šileika"
+                    desc="tyngėjau sugalvoti citatą"
+                />
+                <ClassCard
+                    title="Miglė Tautkutė"
                     desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Jonas Trasauskas"
+                    desc="Visada reikia tikėti savimi"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Rusnė Vaitekūnaitė"
+                    desc="another day another slay"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Livita Valintelytė"
+                    desc="tyngėjau sugalvoti citatą"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Viktorija Varžinskaitė"
+                    desc="life is soup and i am fork"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Neilas Zaborskis"
+                    desc="once you go black you never go back"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Gabriela Zamalytė"
+                    desc="When life gives you lemonade, make lemons. Life will be all like, “What?!”"
                 />
                 <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
-                />
-                <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
-                />
-                <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
-                />
-                <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
-                />
-                <ClassCard
-                    title="Vardas Pavarde"
-                    desc="“Nesureikšminkite gyvenimo. Jūs niekada nepaspruksite iš jo gyvas.” -Elbert Hubbard"
+                    title="Žemyna Zavistauskaitė"
+                    desc="tyngėjau sugalvoti citatą"
                 />
             </div>
         </div>
