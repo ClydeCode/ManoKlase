01eee11f2a8f190a5a2e3f7a9810e21bc307eeae
Clyde <noneclyde@gmail.com>
2023-05-15 20:55:33 +0300

    Update news date displaying position

--- src/pages/News.js
+++ src/pages/News.js
@@ -37,7 +37,7 @@ export default function News() {
             const element = scrollDiv.current.childNodes[n].childNodes[0];
             const date = element.id.substring(0, 7);
 
-            arr.push({"position": element.offsetTop - 266, "date": date});
+            arr.push({"position": element.offsetTop - 290, "date": date});
         }
 
         positionsAndDates.current = arr;
